e5e9e73eef5902f65725bd1515c92c3dc8dcd589
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2013-03-13 15:10:05 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+setup(
+    name='django-turbolinks',
+    version='0.0.1',
+    description='Drop-in turbolinks implementation for Django.',
+    long_description=open('README.md').read(),
+    author='Dmitry Gladkov',
+    author_email='dmitry.gladkov@gmail.com',
+    url='https://github.com/dgladkov/django-turbolinks',
+    packages=['turbolinks'],
+    license='MIT',
+    classifiers=(
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+    ),
+)
