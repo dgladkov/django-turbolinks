5288c7e859c3892235a3793eb481fd9b659bdd2b
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2014-07-03 17:25:19 +0300

    update django middleware code

--- turbolinks/middleware.py
+++ turbolinks/middleware.py
@@ -33,27 +33,36 @@ def same_origin(current_uri, redirect_uri):
 
 class TurbolinksMiddleware(object):
 
+    def process_request(self, request):
+        referrer = request.META.get('HTTP_X_XHR_REFERER')
+        if referrer:
+            # overwrite referrer
+            request.META['HTTP_REFERER'] = referrer
+        return request
+
     def process_response(self, request, response):
-        referrer = request.META.get('HTTP_X_XHR_REFERER', None)
+        referrer = request.META.get('HTTP_X_XHR_REFERER')
         if not referrer:
             # turbolinks not enabled
             return response
 
-        method = request.COOKIES.get('request_method', None)
+        method = request.COOKIES.get('request_method')
         if not method or method != request.method:
             response.set_cookie('request_method', request.method)
+
         if response.has_header('Location'):
             # this is a redirect response
             loc = response['Location']
             request.session['_turbolinks_redirect_to'] = loc
+
             # cross domain redirect
             if referrer and not same_origin(loc, referrer):
-                response.status_code = 403
+                response.status_code = 200
+                response.content = (
+                    '<body><script>location.href="%s"</script></body>'
+                ) % loc
         else:
-            if request.session.get('_turbolinks_redirect_to', None):
+            if request.session.get('_turbolinks_redirect_to'):
                 loc = request.session.pop('_turbolinks_redirect_to')
                 response['X-XHR-Redirected-To'] = loc
         return response
-
-
-
